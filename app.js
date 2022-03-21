531b17b56f4a41903990d200ee6c730a6b688792
Hannah Borel <hannahborel>
2022-03-21 11:44:54 -0500

    menu and banner section complete.  Product section to do: add dynamic sizing

---
+++ app.js
@@ -0,0 +1,116 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let chooseProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    // change current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    // change the choose product
+    chooseProduct = products[index];
+
+    // change text of current  product
+    currentProductImg.src = chooseProduct.colors[0].img;
+    currentProductTitle.innerText = chooseProduct.title;
+    currentProductPrice.innerText = "$" + chooseProduct.price;
+
+    // update color
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = chooseProduct.colors[index].code;
+    });
+
+    //update img on click
+
+    currentProductColors.forEach((color, index) => {
+      color.addEventListener("click", () => {
+        currentProductImg.src = chooseProduct.colors[index].img;
+      });
+    });
+  });
+});
