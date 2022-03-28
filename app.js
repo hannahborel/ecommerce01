30a10653721daaa3e2bdfee91e7056ff213c30fd
Hannah Borel <hannahborel>
2022-03-28 13:57:46 -0500

    Redesign with new background graphics/herolayout and items added

--- app.js
+++ app.js
@@ -1,96 +1,121 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
+const menuMarker = document.getElementById("navMarker");
+let menuCount = 0;
+
+menuMarker.style.left = menuItems[0].offsetLeft + "px";
+menuMarker.style.width = menuItems[0].offsetWidth + "px";
 
 const products = [
   {
     id: 1,
-    title: "Air Force",
+    title: "Superrep",
     price: 119,
     colors: [
       {
-        code: "black",
-        img: "./img/air.png",
+        code: "#bbe453",
+        img: "./img/supeRepGreen.png",
       },
       {
         code: "darkblue",
-        img: "./img/air2.png",
+        img: "./img/supeRepWhite.png",
       },
     ],
   },
   {
     id: 2,
-    title: "Air Jordan",
+    title: "Crater",
     price: 149,
     colors: [
       {
         code: "lightgray",
-        img: "./img/jordan.png",
+        img: "./img/craterWhite.png",
       },
       {
-        code: "green",
-        img: "./img/jordan2.png",
+        code: "black",
+        img: "./img/craterBlack.png",
       },
     ],
   },
   {
     id: 3,
-    title: "Blazer",
+    title: "Free Run",
     price: 109,
     colors: [
       {
-        code: "lightgray",
-        img: "./img/blazer.png",
+        code: "white",
+        img: "./img/nikeFreeWhite.png",
       },
       {
-        code: "green",
-        img: "./img/blazer2.png",
+        code: "black",
+        img: "./img/nikeFreeBlack.png",
       },
     ],
   },
   {
     id: 4,
-    title: "Crater",
-    price: 129,
+    title: "Presto",
+    price: 99,
     colors: [
       {
-        code: "black",
-        img: "./img/crater.png",
+        code: "white",
+        img: "./img/prestoColor.png",
       },
       {
-        code: "lightgray",
-        img: "./img/crater2.png",
+        code: "black",
+        img: "./img/prestoBlack.png",
       },
     ],
   },
   {
     id: 5,
-    title: "Hippie",
+    title: "Pegasus",
     price: 99,
     colors: [
       {
-        code: "gray",
-        img: "./img/hippie.png",
+        code: "#bbe453",
+        img: "./img/pegasusNeon.png",
       },
       {
-        code: "black",
-        img: "./img/hippie2.png",
+        code: "white",
+        img: "./img/pegasusWhite.png",
       },
     ],
   },
 ];
-
 let chooseProduct = products[0];
-
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".size");
 
+const leftArrow = document.getElementById("left");
+const rightArrow = document.getElementById("right");
+
+// Slide Menu With Arrows
+
+leftArrow.addEventListener("click", () => {
+  if (menuCount > 0) {
+    menuCount--;
+    slideMenu(menuItems[menuCount], menuCount);
+  }
+});
+rightArrow.addEventListener("click", () => {
+  if (menuCount < 4) {
+    ++menuCount;
+    slideMenu(menuItems[menuCount], menuCount);
+  }
+});
+
+// EVENT LISTENER FOR MENU ITEMS
+
 menuItems.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    // change current slide
-    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+  // trigger marker and wrapper slide
+  element.addEventListener("click", (e) => {
+    // reset count for arrows
+    menuCount = index;
+    slideMenu(element, index);
 
     // change the choose product
     chooseProduct = products[index];
@@ -104,13 +129,53 @@ menuItems.forEach((element, index) => {
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = chooseProduct.colors[index].code;
     });
-
-    //update img on click
-
-    currentProductColors.forEach((color, index) => {
-      color.addEventListener("click", () => {
-        currentProductImg.src = chooseProduct.colors[index].img;
-      });
-    });
   });
+});
+
+const slideMenu = (element, index) => {
+  menuMarker.style.left = element.offsetLeft + "px";
+  menuMarker.style.width = element.offsetWidth + "px";
+  wrapper.style.transform = `translateX(${-100 * index}vw)`;
+};
+
+//update img on click
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    console.log("click");
+    currentProductImg.src = chooseProduct.colors[index].img;
+  });
+});
+
+// EVENT LISTENERS FOR FEATURED
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      console.log(size, "Inner");
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+    console.log(size, "outer");
+  });
+});
+
+// MODAL
+
+const paymentModal = document.querySelector(".payment");
+const checkoutButton = document.querySelector(".payButton");
+
+const openModal = () => {
+  console.log("openModal() triggered");
+  paymentModal.style.display = "flex";
+};
+
+const closeModal = () => {
+  paymentModal.style.display = "none";
+};
+
+checkoutButton.addEventListener("click", () => {
+  paymentModal.style.display = "none";
 });
